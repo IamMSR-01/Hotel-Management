7187579a0f971c9aae58da03313ada3ef6fc6604
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2025-03-19 00:16:16 +0530

    changes in DB calls

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import { app } from "./app.js";
 
 // dotenv configuration
 dotenv.config({ path: "./env" });
-
+const PORT = process.env.PORT || 3000;
 // database connection functions called
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 3000, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error(`Database connection fialed error: ${error}`);
-    process.exit(1);
-  });
+// connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT || 3000, () => {
+//       console.log(`Server running on port ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error(`Database connection fialed error: ${error}`);
+//     process.exit(1);
+//   });
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB()
+})
