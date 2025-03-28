8462e770cadf660dd50670c71bee34f00801867a
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2025-03-28 17:38:20 +0530

    update room controller add

--- src/controllers/room.controller.js
+++ src/controllers/room.controller.js
@@ -84,4 +84,65 @@ const getRoomBySlug = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, { room }, "Room retrieved  successfully"));
 });
 
-export { addRoom, getAllRooms, getRoomBySlug };
+const updateRoom = asyncHandler(async (req, res) => {
+  const { title, description, price, maxGuests, amenities, location } =
+    req.body;
+
+  const { slug } = req.params;
+
+  if (
+    !title ||
+    !description ||
+    !price ||
+    !maxGuests ||
+    !amenities ||
+    !location
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  if (!slug) {
+    throw new ApiError(400, "Slug is required");
+  }
+
+  const room = await Room.findOne({ slug });
+  if (!room) {
+    throw new ApiError(404, "Room not found");
+  }
+
+  // update slug if title is changed
+  const newSlug = slugify(title, { lower: true });
+  const slugExists = await Room.findOne({
+    slug: newSlug,
+    _id: { $ne: room._id },
+  });
+  if (slugExists) {
+    throw new ApiError(400, "A room with this title already exists");
+  }
+
+  if (req.files && req.files.length > 0) {
+    const uploadedImages = [];
+    for (const file of req.files) {
+      const uploaded = await uploadOnCloudinary(file.path);
+
+      if (uploaded) uploadedImages.push(uploaded.url);
+    }
+    room.images = [...room.images, ...uploadedImages];
+  }
+
+  room.title = title || room.title;
+  room.slug = newSlug || slug;
+  room.description = description || room.description;
+  room.price = price || room.price;
+  room.maxGuests = maxGuests || room.maxGuests;
+  room.amenities = amenities || room.amenities;
+  room.location = location ? JSON.parse(location) : room.location;
+
+  await room.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { room }, "Room updated successfully"));
+});
+
+export { addRoom, getAllRooms, getRoomBySlug, updateRoom };
