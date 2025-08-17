7e6c3232123eb7fa960b3cb53e90454dd760cacf
Mohd Shaqib Raza <iammsr19413m@gmail.com>
2025-08-17 17:59:00 +0530

    fix: Correct environment variable name for MongoDB URI in database connection and webhook logging

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/hotel-booking`);
         console.log(`\n MongoDB connected successfully: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB connection fialed error: " + error);
